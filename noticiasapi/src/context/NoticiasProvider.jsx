7dd2b554d0e94adda3611cae27f24a8b539d1d8b
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-08-31 18:54:35 -0500

    creando el context

---
+++ noticiasapi/src/context/NoticiasProvider.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+
+import {  createContext } from "react";
+
+const NoticiasContext = createContext()
+
+const NoticiasProvider = ({children}) => {
+
+    return(
+        <NoticiasContext.Provider
+            value={{
+
+            }}
+        >
+            {children}
+        </NoticiasContext.Provider>
+    )
+}
+
+export {
+    NoticiasProvider
+}
+
+export default NoticiasContext
