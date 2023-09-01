2a7ca7ee161fb44e119e3f7107493a379a87bddf
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-09-01 01:44:21 -0500

    consultando según categoria

--- noticiasapi/src/context/NoticiasProvider.jsx
+++ noticiasapi/src/context/NoticiasProvider.jsx
@@ -1,12 +1,26 @@
 /* eslint-disable react/prop-types */
 
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
+import axios from 'axios'
 
 const NoticiasContext = createContext()
 
 const NoticiasProvider = ({children}) => {
 
     const [categoria, setCategoria] = useState('general')
+    const [noticias, setNoticias] = useState([])
+    useEffect(() => {
+        const consultarAPI = async () => {
+            try { 
+                const url = `https://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+                const { data } = await axios(url)
+                setNoticias(data.articles)
+            } catch(error) {
+                console.log(error)
+            }
+        } 
+        consultarAPI()
+    }, [categoria])
 
     const handleChangeCategoria = e => {
         setCategoria(e.target.value)
